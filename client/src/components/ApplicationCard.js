dbf2eb7a8a9b4934150846f9a8a48662013ce4d5
Tom Pies <tombo@Toms-MacBook-Pro.local>
2021-03-07 16:17:32 +0000

    diff date working

--- client/src/components/ApplicationCard.js
+++ client/src/components/ApplicationCard.js
@@ -19,17 +19,18 @@ const ApplicationCard = ({ application, removeApplication }) => {
   };
 
     var date1 = new Date (today);
-    var date2 = new Date ("03/08/2021");
+    var date2 = new Date (application.closingDate);
     var Diff_In_Days = (date2.getTime() - date1.getTime())/ (1000 * 3600 * 24);
+    console.log(application.closingDate);
     
     console.log("Diff in days is", Diff_In_Days)
 
-    
+
   return (
     <>
       <h1>{application.company}</h1>
-      <p>Days left to closing:{today}</p>
-      <p>closing date:{application.closingDate}</p>
+      <p>Days left to closing:{Diff_In_Days}</p>
+
 
       <button onClick={handleDelete}> 🗑 </button>
       <hr></hr>
