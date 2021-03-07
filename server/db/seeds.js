7d52be0e95445ebc1a054cdc1349db9ec0b6662d
Tom Pies <tombo@Toms-MacBook-Pro.local>
2021-03-07 12:22:13 +0000

    boilerplate customised

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -4,7 +4,9 @@ db.dropDatabase();
 db.applications.insertMany([
   {
     company: "Tom's Dev Co.",
+    url: "http://urltojobhere.co.uk",
     role: "Junior Dev",
-    closing_date: "tbc"
+    closing_date: "tbc",
+    email: "mrhiringmanager@job.com",
   },
 ]);
